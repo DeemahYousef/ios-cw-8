ee72eaea97e5ed142b8ed4031e2cb91096c4f527
DeemahYousef <dalkhaledi12345@hotmail.com>
2022-03-12 11:07:12 +0300

    done

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+var greeting = "Hello, playground"
+var name = "Deemah"
+
+var birthdayYear = 2005
+
+var currentYear = 2022
+
+var age = 2022-birthdayYear
+
+print("Hello my name is \(name)my age is \(age)")
+
+var num1 = "8"
+
+var num2 = 4
+
+var num3 = 2.5
+
+num3 = Double(num1) ?? 0 + Double(num2)
